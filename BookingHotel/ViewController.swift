9bb7b8c8ba5d29660e49e90b74105cffe3efa476
Denim <dagn456@mail.ru>
2023-08-31 09:37:15 +0400

    Initial Commit

---
+++ BookingHotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookingHotel
+//
+//  Created by Деним Мержан on 31.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
