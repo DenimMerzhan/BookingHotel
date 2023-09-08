b1a57953844f09d40f65d339690844f46cc17813
Denim <dagn456@mail.ru>
2023-09-08 17:43:38 +0400

    Добавлена маска номера

--- BookingHotel/BookingNumber/View/Cell/InfoTouirist/InfoTouirist.swift
+++ BookingHotel/BookingNumber/View/Cell/InfoTouirist/InfoTouirist.swift
@@ -8,35 +8,27 @@
 import UIKit
 
 class InfoTouirist: UITableViewCell {
-
+    
     
     @IBOutlet weak var upPlaceHolder: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var view: UIView!
     
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         view.layer.cornerRadius = 10
         textField.borderStyle = .none
-        
         textField.delegate = self
         upPlaceHolder.isHidden = true
     }
-
+    
+    override func prepareForReuse() {
+        upPlaceHolder.isHidden = true
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-    
-    @IBAction func textiFieldDidChanged(_ sender: UITextField) {
-        if sender.text?.count == 0 {
-            upPlaceHolder.isHidden = true
-        }else {
-            upPlaceHolder.isHidden = false
-        }
+        
     }
     
 }
@@ -44,7 +36,42 @@ class InfoTouirist: UITableViewCell {
 
 extension InfoTouirist: UITextFieldDelegate {
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = textField.text else {
+            return false
+        }
+        let newString = (text as NSString).replacingCharacters(in: range, with: string)
+        textField.text = formatter(mask: "+X (XXX) XXX XX-XX", phoneNumber: newString)
         upPlaceHolder.text = textField.placeholder
+        
+        return false
+    }
+    
+    func formatter(mask:String,phoneNumber: String) -> String {
+        
+        let number = phoneNumber.replacingOccurrences(of: "[^0-9]", with: "",options: .regularExpression)
+        
+        var result = ""
+        var index = number.startIndex
+        
+        for character in mask where index < number.endIndex {
+            if character == "X" {
+                result.append(number[index])
+                index = number.index(after: index)
+                
+            }else {
+                result.append(character)
+            }
+            
+        }
+        
+        if result == "" {
+            upPlaceHolder.isHidden = true
+        }else {
+            upPlaceHolder.isHidden = false
+        }
+        
+        return result
     }
 }
